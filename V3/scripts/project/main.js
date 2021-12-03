d359d3af8ba6614f646e6714e9472553f7e19864
Kartik Singh <kartik.singh4f@gmail.com>
2021-12-03 17:39:31 +0530

    alert for data test

--- V3/scripts/project/main.js
+++ V3/scripts/project/main.js
@@ -30,6 +30,7 @@ document.addEventListener("message", handleEvent);
 function handleEvent(messageTemp) {	
 	isWeb = false;
 	messageData =  messageTemp.data;	
+	alert("Data: "+JSON.stringify(messageData))
 }
 
 runOnStartup(async runtime =>
